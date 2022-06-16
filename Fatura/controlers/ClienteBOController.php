0c52910be895484498055a67ee23a4e348e28d31
GoncaloVital <2201099@my.ipleiria.pt>
2022-06-16 22:01:00 +0100

    delete

--- Fatura/controlers/ClienteBOController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class ClienteBOController extends BaseController
-{
-    public function index()
-    {
-        $this->makeView('dashboardClientBO', 'index');
-    }
-}
--- Fatura/models/Vigor.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Vigor extends \ActiveRecord\Model
-{
-    static $has_many = array(
-        array('iva')
-    );
-}
